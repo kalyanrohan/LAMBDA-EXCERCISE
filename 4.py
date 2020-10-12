61476ddb40d45cc2307daf8bf85b60495289aed3
kalyanrohan <sri.rohan@binus.ac.id>
2020-10-12 19:06:00 +0700

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,15 +1,12 @@
 import datetime
-datetime=str(datetime.datetime.now())
-extract= lambda y:y.split()
-extract2= lambda x:x.split("-")
-extract3=(extract2(str(extract(datetime)))
-year1=extract3[0]
-month1=extract3[1]
-day1=extract3[2]
-# time=extract3[3]
-print(f"year={year1}")
-print(f"month={month1}")
-print(f"day={day1}")
-# print(f"time={time}")
-
+now = datetime.datetime.now()
+print(now)
+year = lambda x: x.year
+month = lambda x: x.month
+day = lambda x: x.day
+t = lambda x: x.time()
+print(year(now))
+print(month(now))
+print(day(now))
+print(t(now))
 
