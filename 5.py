11d06ee618ea2ad601f229ee9e744dd645cd7d30
kalyanrohan <sri.rohan@binus.ac.id>
2020-10-14 15:07:54 +0700

    Update 5.py

--- 5.py
+++ 5.py
@@ -6,3 +6,16 @@ def fibonacci(x):
     return fib_list[:x] 
   
 print(fibonacci(eval(input())))
+
+from functools import reduce
+#The reduce(fun,seq) function is used to apply a particular function passed in its argument to all of the list elements mentioned in the sequence passed along.
+fib =  lambda n: reduce(lambda x, _: x+[x[-1]+x[-2]],range(n-2), [0, 1])
+n = eval(input("Enter the number of terms: "))
+print(fib(n))
+
+def fibbonaci_long(n):
+    result = [0, 1]
+    for i in range(n-2):
+        result.append(result[-1] + result[-2])
+    return result
+
